665969a3092ae0027c38bd0a4070a41382151e21
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-09-21 19:31:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,60 +6,61 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","6435225"))
-API_HASH = getenv("API_HASH","4e984ea35f854762dcde906dce426c2d")
-BOT_TOKEN = getenv("BOT_TOKEN","6674525999:AAEdPCxpdz2k8XY1VLSyAdeNMV3I4Wfsvv8")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "9999"))
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002082157755"))
-BOTADDLOGS = int(getenv("BOTADDLOGS", "-1002082157755")) # LOGGER_ID Id Also Use No Problem
-GBAN_LOGS = int(getenv("GBAN_LOGS", "-1002082157755"))
-GCAST_USERS = list(map(int, getenv("GCAST_USERS", "2105971379 5916859256").split()))
-OWNER_ID = int(getenv("OWNER_ID", 2105971379))
+
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
+
+LOGGER_ID = int(getenv("LOGGER_ID", -1002082157755))
+
 OWNERS = list(map(int, getenv("OWNERS", "1280494242").split()))
-OWNER_USERNAME = getenv("OWNER_USERNAME","fake_one")
+
+OWNER_ID = int(getenv("OWNER_ID", 1280494242))
+
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/AbhiModszYT/AnieXEricaMusic")
+
+HEROKU_API_KEY = getenv("HEROKU_API_KEY","HK543fklqxgt66hvxf")
+
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/AbhiModszYT/AnieXEricaMusic",
+)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GIT_TOKEN = getenv("GIT_TOKEN",None)
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+)  
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Mahi_Botz")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mr_mahiji")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Mr_Mahiji")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
-AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "5400"))
-SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", 7000))
-SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", 7000))
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "9999")) 
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "2a230af10e0a40638dc77c1febb47170")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "7f92897a59464ddbbf00f06cd6bda7fc")
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
-SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 5242880000))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 524288000000))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 5242880000000))
+
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
-STRING6 = getenv("STRING_SESSION6", None)
+
+
 BANNED_USERS = filters.user()
 adminlist = {}
 lyrical = {}
 votemode = {}
 autoclean = []
 confirmer = {}
-AMBOT = [
-    "🔎",
-    "🎶",
-    "🧪",
-    "ᴘʟꜱ ᴡᴀɪᴛ..",
-    "ᴘʀᴏᴄᴇꜱꜱɪɴɢ..",
-]
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
+
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/4e68b6d15e042249fb284.png")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/file/4e68b6d15e042249fb284.png")
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = getenv("STATS_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
+STATS_IMG_URL = getenv("STATS_IMG_URL", "https://telegra.ph/file/4e68b6d15e042249fb284.png")
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
@@ -68,7 +69,6 @@ YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
-
 
 def time_to_seconds(time):
     stringt = str(time)
